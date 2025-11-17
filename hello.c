0245d5ac1317f29bdce3610c52de33a5606f5c1a
Rabi Gurung <rabi.gurung@yokogawa.com>
2025-11-17 09:38:30 -0700

    Backup

--- hello.c
+++ hello.c
@@ -3,18 +3,5 @@
 int main()
 {
 	printf("This is my first ANSI C program");
-    // Add variables and calculations
-    int number1 = 10;
-    int number2 = 5;
-    int sum = number1 + number2;
-    
-    printf("The sum of %d and %d is: %d\n", number1, number2, sum);
-    
-    // Add user input
-    int userNumber;
-    printf("Enter a number: ");
-    scanf("%d", &userNumber);
-    printf("You entered: %d\n", userNumber);
-    
-    return 0;
+	return 0;
 }
